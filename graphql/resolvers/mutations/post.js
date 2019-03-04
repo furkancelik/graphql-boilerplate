a7032cdb3ef67ac5a404d8a7a504e6761d8977ce
Furkan Çelik <furkancelik@Furkan-MacBook-Pro.local>
2019-03-04 05:32:48 +0300

    Authorized Post Add

--- graphql/resolvers/mutations/post.js
+++ graphql/resolvers/mutations/post.js
@@ -1,7 +1,17 @@
 export default {
-  createPost: async (parent, args, { Post }) => {
+  createPost: async (
+    parent,
+    { data: { title, desciption } },
+    { Post, activeUser }
+  ) => {
+    if (!activeUser) throw new Error("You are not authenticated!");
+    const user = activeUser.id;
     try {
-      return await Post(args.data).save();
+      return await Post({
+        title,
+        desciption,
+        user
+      }).save();
     } catch (e) {
       throw new Error(e);
     }
