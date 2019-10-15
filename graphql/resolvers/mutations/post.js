c922ba48706216a8caa82388cf3304b09f6f767a
Furkan Çelik <furkancelik@Furkan-MacBook-Pro.local>
2019-10-16 02:41:07 +0300

    edit post

--- graphql/resolvers/mutations/post.js
+++ graphql/resolvers/mutations/post.js
@@ -15,5 +15,30 @@ export default {
     } catch (e) {
       throw new Error(e);
     }
+  },
+
+  editPost: async (
+    parent,
+    { data: { id, title, desciption } },
+    { Post, activeUser }
+  ) => {
+    if (!activeUser) throw new Error("You are not authenticated!");
+    try {
+      const post = await Post.findById(id);
+      if (post.user.toString() === activeUser.id.toString()) {
+        const update = await Post.findByIdAndUpdate(id, {
+          $set: { title, desciption }
+        });
+        if (update) {
+          return await Post.findById(id);
+        } else {
+          throw new Error("Bir hata meydana geldi!");
+        }
+      } else {
+        throw new Error("Yetkiniz olmayan gönderiyi güncelleyemezsiniz!");
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 };
